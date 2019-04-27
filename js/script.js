b4430e287859da099be565a5caf6f391747f698a
Kinga Jackowska <jackowska.kinga@gmail.com>
2019-04-27 20:50:24 +0200

    commit message

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// link
+var url = "https://restcountries.eu/rest/v1/name/";
+var countriesList = document.getElementById("countries");
+
+// nasłuchiwanie klika na guzik
+document.getElementById("search").addEventListener("click", searchCountries);
+
+// zapytanie o nazwę kraju
+function searchCountries() {
+	var countryName = document.getElementById("country-name").value;
+	if (!countryName.length) countryName = "Poland";
+	fetch(url + countryName)
+		.then(function(resp) {
+		return resp.json();
+	})
+		.then(showCountriesList);
+}
+
+// wypisanie listy wyników
+function showCountriesList(resp) {
+	countriesList.innerHTML = "";
+
+	resp.forEach(function(item) {
+		var liEl = document.createElement("li");
+		liEl.innerText = item.name;
+		countriesList.appendChild(liEl);
+	});
+}
